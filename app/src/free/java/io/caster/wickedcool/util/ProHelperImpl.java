010303c4221b16ac0e1fb9b2bf40a01fc272098f
Donn Felker <donn@donnfelker.com>
2015-02-25 17:43:49 -0700

    Adding free and pro versions and adding prohelper and impls

---
+++ app/src/free/java/io/caster/wickedcool/util/ProHelperImpl.java
@@ -0,0 +1,10 @@
+package io.caster.wickedcool.util;
+
+
+public class ProHelperImpl implements ProHelper {
+    @Override
+    public boolean isPro() {
+        // This is the free version man, not the pro version!!!!
+        return false;
+    }
+}
---
+++ app/src/pro/java/io/caster/wickedcool/util/ProHelperImpl.java
@@ -0,0 +1,11 @@
+package io.caster.wickedcool.util;
+
+import io.caster.wickedcool.util.ProHelper;
+
+public class ProHelperImpl implements ProHelper {
+    @Override
+    public boolean isPro() {
+        // Heck yes, this is the pro version.
+        return true;
+    }
+}
